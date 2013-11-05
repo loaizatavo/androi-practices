8ca0828391029525757174ea1ff0e64d4db90c83
Gustavo Loaiza Robles <loaizatavo@gmail.com>
2013-11-05 15:59:42 -0600

    creada la clase Crime

--- CriminalIntent/src/com/tavo/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/com/tavo/criminalintent/CrimeActivity.java
@@ -1,22 +1,14 @@
 package com.tavo.criminalintent;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
 
-public class CrimeActivity extends Activity {
+public class CrimeActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.crime, menu);
-		return true;
 	}
 
 }
