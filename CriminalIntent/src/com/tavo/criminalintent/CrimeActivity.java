680202427d378108b01d353ec905487d81531b75
Gustavo Loaiza <gustavo@Gustavos-MacBook-Pro.local>
2013-11-05 21:20:58 -0600

    Finalizado el capitulo de 7 de fragments

--- CriminalIntent/src/com/tavo/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/com/tavo/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.tavo.criminalintent;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -9,6 +11,16 @@ public class CrimeActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+		
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		if (fragment == null) {
+			//importante: aqui se asocia la instancia del fragment
+			fragment = new CrimeFragment();
+			fm.beginTransaction()
+				.add(R.id.fragmentContainer, fragment)
+				.commit();
+		}
 	}
 
 }
---
+++ CriminalIntent/src/com/tavo/criminalintent/CrimeFragment.java
@@ -0,0 +1,48 @@
+package com.tavo.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+	private Crime mCrime;
+	private EditText mTitleField;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mCrime = new Crime();
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+		
+		mTitleField = (EditText)v.findViewById(R.id.crime_title);
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				mCrime.setTitle(s.toString());
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// no se hace nad apor el momento
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// Tampoco se hace nada por ahora
+			}
+		});
+		
+		return v;
+	}	
+}
